c05dbb2d5cef2c61dbea890a2400b688e93d73a4
Rozovod <rozovod.store@mail.ru>
2022-12-14 15:41:54 +0300

    8. Скачивание файла с ограничением. [#144271 #369640]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -8,26 +8,33 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String outFile;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String outFile) {
         this.url = url;
         this.speed = speed;
+        this.outFile = outFile;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream out = new FileOutputStream(outFile)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            int downloadData = 0;
+            long timeBegin = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long timeBegin = System.currentTimeMillis();
-                out.write(dataBuffer, 0, bytesRead);
-                long timeEnd = System.currentTimeMillis();
-                long timeReal = timeEnd - timeBegin;
-                if (timeReal < speed) {
-                    Thread.sleep(speed - timeReal);
+                downloadData += bytesRead;
+                if (downloadData == speed) {
+                    long timeReal = System.currentTimeMillis() - timeBegin;
+                    if (timeReal < 1000) {
+                        Thread.sleep(1000 - timeReal);
+                    }
+                    downloadData = 0;
+                    timeBegin = System.currentTimeMillis();
                 }
+                out.write(dataBuffer, 0, bytesRead);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,10 +43,18 @@ public class Wget implements Runnable {
         }
     }
 
+    private static void validationArgs(String[] args) {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Введены не все параметры.");
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        validationArgs(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String outFile = args[2];
+        Thread wget = new Thread(new Wget(url, speed, outFile));
         wget.start();
         wget.join();
     }
