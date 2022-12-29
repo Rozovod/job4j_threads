5ee9bd95808c63bdfea83895588e1c24fb152dd3
Rozovod <rozovod.store@mail.ru>
2022-12-29 15:41:41 +0300

    3. Junit тест для блокирующей очереди. [#68589]

---
+++ src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.concurrent;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenAdd7AndPoll5() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(7);
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        for (int i = 1; i <= 7; i++) {
+                            queue.offer(i);
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    try {
+                        for (int i = 1; i <= 5; i++) {
+                            buffer.add(queue.poll());
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isEqualTo(Arrays.asList(1, 2, 3, 4, 5));
+    }
+}
